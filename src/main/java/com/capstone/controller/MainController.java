8bc151bb08940e1ab39e77904a07041328335fd1
ggs4029 <ggs65429@gmail.com>
2024-03-24 15:53:46 +0900

    화면구현2 #1

--- src/main/java/com/capstone/controller/MainController.java
+++ src/main/java/com/capstone/controller/MainController.java
@@ -9,14 +9,49 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 public class MainController {
 
-	@GetMapping("/main/main")
+	@GetMapping("main")
 	public String main() {
 		log.info("Main controller");
-		return "main/main";
+		return "main";
 	}
 
+	@GetMapping("board/newBoard")
+    public String writeBoard(){
+        return "board/newBoard";
+    }
+
+	@GetMapping("member/memberEdit")
+    public String memberEdit(){
+        return "member/memberEdit";
+    }
+
+	@GetMapping("member/myBoard")
+    public String myBoard(){
+        return "member/myBoard";
+    }
+
+	@GetMapping("board/example3")
+    public String example3(){
+        return "board/example3";
+    }
+
+	@GetMapping("board/example2")
+    public String example2(){
+        return "board/example2";
+    }
+
 	@GetMapping("board/example1")
-    public String edit() {
+    public String board() {
         return "board/example1";
+    }
+
+	@GetMapping("member/edit3")
+    public String edit3(){
+        return "member/edit3";
+    }
+
+	@GetMapping("member/edit2")
+    public String edit2(){
+        return "member/edit2";
     }
 }
